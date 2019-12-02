bfca0271c6bea039fbd14d07192bff56d998e547
tiagombp <tiagombp@gmail.com>
2019-12-02 17:54:52 -0300

    Update prepara_dados_vis.R

--- R/prepara_dados_vis.R
+++ R/prepara_dados_vis.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 load("R/Garantias.Rdata")
 
-dados_vis <- agrupador_total %>%
+dados_vis_pre <- agrupador_total %>%
   filter(Periodo == max(Periodo),
          Inicio != Classificador) %>%
   select(Inicio, Classificador, valor = total_total) %>%
@@ -10,9 +10,11 @@ dados_vis <- agrupador_total %>%
     str_replace(str_replace_all(as.character(valor), "\\.", ""), ",", "\\.")),
     rank_geral = rank(-valor))
 
-dados_vis_summ <- dados_vis %>%
+dados_vis <- dados_vis_pre %>%
   group_by(Classificador) %>%
   summarise(total_classificador = sum(valor)) %>%
   ungroup() %>%
-  mutate(rank_classificadores = rank(-total_classificador)) 
-  
+  mutate(rank_classificadores = rank(-total_classificador)) %>%
+  right_join(dados_vis)
+
+write.csv(dados_vis, file = "webpage/dados_vis.csv")
