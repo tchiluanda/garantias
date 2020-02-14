e8b8bc7941eb55643dfe430b46c4db7b6646e9d2
tiagombp <tiagombp@gmail.com>
2020-02-14 16:54:19 -0300

    experimentos do grid

---
+++ gera_posicoes_palavras.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(readxl)
+library(jsonlite)
+
+grid <- read_excel("grid_letras.xlsx", sheet = "export_4")
+
+new_grid <- as.data.frame(grid[,-1])
+
+gera_grid <- function(grid_inicial) {
+  
+  coordenadas <- data.frame()
+  
+  n <- 1
+  
+  for (x in 1:dim(grid_inicial)[2]) {
+    for (y in 1:dim(grid_inicial)[1]) {
+      if (!is.na(grid_inicial[y,x])) {
+        #print(paste(y,x))
+        coordenadas[n,1] <- x
+        coordenadas[n,2] <- y
+        n <- n+1
+      }
+    }
+  }
+  
+  coordenadas_export <- coordenadas %>%
+    rename(x = V1,
+           y = V2) %>%
+    mutate(nome = paste("bolha", row_number()))
+  
+  return(coordenadas_export)
+}
+
+
+exportar <- gera_grid(new_grid)
+
+ggplot(exportar, aes(x = x, y = y)) + geom_point()
+
+jsonlite::write_json(exportar, "grid_temp.json")
