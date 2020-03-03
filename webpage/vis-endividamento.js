3115c0cfc7cfabf2b3e9f5cddf41add0629b9c2e
tiagombp <tiagombp@gmail.com>
2020-03-03 17:31:18 -0300

    inicia tentativa de scrolly

--- webpage/vis-endividamento.js
+++ webpage/vis-endividamento.js
@@ -28,16 +28,35 @@ d3.csv("dividas_totais.csv").then(function(dados) {
 
   console.table(dados);
 
-  // filtra, gera uma versão "objetificada"
+  // funcaozinha para gerar rotulos
 
-  const dados_total = dados.filter(d => d.Escopo != "Total" &&
-                                        d.tipo_divida != "Divida_Total");
-  const dados_flat = {};
-  for (el of dados) {
-    dados_flat[el.tipo_divida] = el.valor;
+  const localeBrasil = {
+    "decimal": ",",
+    "thousands": ".",
+    "grouping": [3],
+    "currency": ["R$", ""]};
+
+  const formataBR = d3.formatDefaultLocale(localeBrasil).format(",.1f");
+
+  const multiplos = [1, 1e3, 1e6, 1e9, 1e12];
+  const sufixo    = ["", "mil", "mi", "bi", "tri"];
+  const obj_mult = multiplos.map((d,i) => ({
+    valor: d,
+    sufixo: sufixo[i]
+  }));
+  console.log("objeto multiplos", obj_mult)
+
+  const valor_formatado = function(x) {
+    for (mult of obj_mult) {
+      const val = x/mult.valor;
+      console.log(val)
+      if (val < 1000) return formataBR(val) + " " + mult.sufixo;
+    }
   }
 
-  console.log(dados_flat);
+  // filtra
+  const dados_total = dados.filter(d => d.Escopo != "Total" &&
+                                        d.tipo_divida != "Divida_Total");
 
   // escala y
   const y = d3.scaleLinear()
@@ -48,24 +67,14 @@ d3.csv("dividas_totais.csv").then(function(dados) {
   const l = d3.scaleLinear()
               .range([0, h - margin.bottom - margin.top])
               .domain([0, d3.max(dados, d => d.valor)]); 
-              
+  
+  const y_max = y(d3.max(dados, d => d.valor));     
+
   // constroi uma array com os dados necessarios 
   // para a visualizacao. essa array que vai ser 
   // "amarrada" aos elementos visuais.
 
-  /* comecei fazendo na mão:
-
-  const dados_vis = [
-    {
-      tipo_divida: "Divida_Total",
-      x : margin.left + (w_liq*1)/5,
-      y : y(0),
-      height: dados_flat[]
-    }, ...
-  ]
-
-  mas dá pra automatizar, em termos: */
-  const bar_width = 16;
+  const bar_width = 20;
   const dados_vis = [];
 
   console.table(dados_total);
@@ -86,9 +95,16 @@ d3.csv("dividas_totais.csv").then(function(dados) {
       x_0 : margin.left +  w_liq/4 - bar_width/2,
       x_1 : margin.left + (w_liq/4)*(4 - x_index) - bar_width/2, //(w_liq/5)*(i+1),
       height : l(dados_total[i].valor),
+      valor_formatado : valor_formatado(dados_total[i].valor),
       y : i == 0 ? y(0) : dados_vis[i-1].y + l(dados_total[i-1].valor)
     })
   };
+
+  // acrescenta as posições finais das barras
+  for (el of dados_vis) {
+    el.x_2 = el.x_1 + 5*bar_width/8 * (el.Escopo == "Estados" ? -1 : +1);
+    el.y_2 = y_max - el.height;
+  }
 
   console.table(dados_vis);
 
@@ -121,6 +137,21 @@ d3.csv("dividas_totais.csv").then(function(dados) {
     .attr("fill", "goldenrod");
   }
 
+  const step_colore = function() {
+    d3.selectAll("rect").transition().duration(500)
+      .attr("fill", d => d.Escopo == "Estados" ? "dodgerblue" : "firebrick");
+  };
+
+  const step_final = function() {
+    d3.selectAll("rect")
+      .transition()
+      .duration(500)
+      .attr("x", d => d.x_2)
+      .transition()
+      .duration(500)
+      .attr("y", d => d.y_2);
+  };
+
   const reverte_step = function(tipo_divida) {
     const bar_atual = d3.selectAll("rect").filter(d => d.tipo_divida == tipo_divida);
     console.log("barra atual", bar_atual);
@@ -133,6 +164,7 @@ d3.csv("dividas_totais.csv").then(function(dados) {
   const reseta = function() {
     d3.selectAll("rect").transition().duration(500)
     .attr("x", d => d.x_0)
+    .attr("y", d => d.y)
     .attr("fill", "#333");
   }
 
@@ -152,6 +184,12 @@ d3.csv("dividas_totais.csv").then(function(dados) {
       case "step-3":
         step("Divida_demais");
         break;
+      case "step-4":
+        step_colore();
+        break;          
+      case "step-5":
+        step_final();
+        break;        
       case "step-0":
         reseta();
         break;
