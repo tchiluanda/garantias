2da26bb28c026cdb9c026ef107f95dfcdb3e8332
tiagombp <tiagombp@gmail.com>
2020-03-05 15:06:43 -0300

    só faltam os últimos rótulos!

--- webpage/vis-endividamento.js
+++ webpage/vis-endividamento.js
@@ -4,9 +4,14 @@ const $svg_endividamento       = $container_endividamento.select('#vis-endividam
 const w_bruto = $container_endividamento.node().offsetWidth;
 const w_endiv = w_bruto >= 600 ? 600 : w_bruto;
 
-const h_endiv = $container_endividamento.node().offsetHeight;
+const h_bruto = $container_endividamento.node().offsetHeight;
+const h_endiv = h_bruto >= 500 ? 500 : h_bruto;
 
-//const h = w < 510 ? 500 : 500;
+// calculada a altura do "viewport", preciso definir as alturas
+// dos elementos que dependem desse valor.
+// como defini como uma variável, vou atualizar simplesmente
+// essa variável aqui.
+d3.select(":root").style("--altura-endiv", h_endiv + "px");
 
 const margin = {
   top: 20,
@@ -30,7 +35,7 @@ d3.csv("dividas_totais.csv").then(function(dados) {
     el.valor = +el.valor;
   }
 
-  console.table(dados);
+  //console.table(dados);
 
   // funcaozinha para gerar rotulos
 
@@ -48,15 +53,16 @@ d3.csv("dividas_totais.csv").then(function(dados) {
     valor: d,
     sufixo: sufixo[i]
   }));
-  console.log("objeto multiplos", obj_mult)
+  //console.log("objeto multiplos", obj_mult)
 
   const valor_formatado = function(x) {
     for (mult of obj_mult) {
       const val = x/mult.valor;
-      console.log(val)
       if (val < 1000) return formataBR(val) + " " + mult.sufixo;
     }
   }
+
+  console.table(dados);
 
   // filtra
   const dados_total = dados.filter(d => d.Escopo != "Total" &&
@@ -80,16 +86,17 @@ d3.csv("dividas_totais.csv").then(function(dados) {
   const y_max = y(d3.max(dados, d => d.valor));     
   const l_max = l(d3.max(dados, d => d.valor));   
 
-
+  //////////
   // constroi uma array com os dados necessarios 
   // para a visualizacao. essa array que vai ser 
   // "amarrada" aos elementos visuais.
 
   const bar_width = 20;
+
   const dados_vis = [];
 
   console.table(dados_total);
-  console.log("Y0", y(0), l(dados_total[0].valor));
+  //console.log("Y0", y(0), l(dados_total[0].valor));
   
   for (let i = 0; i < dados_total.length; i++) {
 
@@ -119,15 +126,75 @@ d3.csv("dividas_totais.csv").then(function(dados) {
 
   console.table(dados_vis);
 
-  // só mais um parâmetro: width
-  
 
+  //////////
+  // agora um objeto para os rótulos
+  // vou implementar os rotulos como um função que é
+  // chamada e inclui o rótulo equivalente ao id na hora.
+  const tipos_dividas = {
+    "Divida_Total" : "Dívida Total de Estados e Municípios",
+    "Divida_Uniao" : "Dívida com a União",
+    "Divida_Garantida" : "Outras dívidas, com garantia da União",
+    "Divida_demais" : "Outras dívidas, sem garantia da União"
+  };
+
+  const rotulos = {};
+
+  for (el of dados) {
+    if (el.Escopo == "Total") {
+      if (el.tipo_divida == "Divida_Total") {
+        rotulos[el.Escopo + "_" + el.tipo_divida] = {
+          x: dados_vis[0].x_0,
+          y: dados_vis[0].y,
+          texto: tipos_dividas[el.tipo_divida],
+          valor : valor_formatado(el.valor),
+          cor : cor(el.Escopo)
+        }
+      } 
+      else {
+        const mini_dados_vis = dados_vis.filter(
+          d => d.tipo_divida == el.tipo_divida);
+          // pegando o [0], pq sempre vou ter dois elementos para cada tipo_divida
+          // em "dados_vis", e me interessa só o primeiro
+        rotulos[el.Escopo + "_" + el.tipo_divida] = {
+          x: mini_dados_vis[0].x_1,
+          y: mini_dados_vis[0].y,
+          texto : tipos_dividas[el.tipo_divida],
+          valor: valor_formatado(el.valor),
+          cor : cor("Destaque") // esses são as barras do
+                                // waterfall
+        }
+      }
+
+    } 
+    else if (el.tipo_divida != "Divida_Total") {
+      // pq não me interessa os valores totais por "Escopo".
+      // aqui agora estou vendo os tipos de divida (Uniao, Garantias, demais)
+      // para cada escopo (Estados e Municipios)
+      const mini_dados_vis = dados_vis.filter(
+        d => d.Escopo == el.Escopo && d.tipo_divida == el.tipo_divida);
+      // o mini_dados_vis resultante aqui vai ser uma array de um objeto só
+      rotulos[el.Escopo + "_" + el.tipo_divida] = {
+        x: mini_dados_vis[0].x_2,
+        y: mini_dados_vis[0].y_2,
+        texto : "",
+        valor : valor_formatado(el.valor),
+        cor : cor(el.Escopo)
+      }        
+    }
+  }
+
+  console.log("Rotulos", rotulos);
+
+
+  
+  //////////
   // binding
   const bars = $svg_endividamento
     .selectAll("rect")
     .data(dados_vis);
   
-  console.log(bars);
+  //console.log(bars);
 
   $svg_endividamento.style("background-color", "cornsilk")
 
@@ -139,27 +206,27 @@ d3.csv("dividas_totais.csv").then(function(dados) {
       .append("rect")
       .classed("d3--endividamento-total", true)
       .attr("x", dados_vis[0].x_0)
-      .attr("y", y(0))
+      .attr("y", y_max)
       .attr("height", 0)
       .attr("width", bar_width)
       .attr("stroke-width", 0)
       .attr("fill", cor("Total"))
       .transition()    
-      .duration(250)
+      .duration(500)
+      .attr("y", y(0))
       .attr("height", l_max);
 
     //uma linha de referência
     $svg_endividamento
       .append("line")
       .attr("x1", margin.left)
-      .attr("x2", margin.left)
+      .attr("x2", margin.left + w_liq)
       .attr("y1", y_max)
       .attr("y2", y_max)
-      .attr("stroke", "#666")
+      .attr("stroke", "transparent")
       .transition()
-      .delay(250)
-      .duration(200)
-      .attr("x2", margin.left + w_liq);
+      .duration(500)
+      .attr("stroke", "#333")
     
     // segmentos de barrinhas
     bars.enter().append("rect")
@@ -169,7 +236,11 @@ d3.csv("dividas_totais.csv").then(function(dados) {
       .attr("height", d => d.height)
       .attr("width", bar_width)
       .attr("stroke-width", 0)
-      .attr("fill", cor("Total"));
+      .attr("fill", cor("Total"))
+      .attr("opacity", 0)
+      .transition()
+      .delay(500)
+      .attr("opacity", 1);
   }
 
   const remove_barra_total = function() {
@@ -178,14 +249,14 @@ d3.csv("dividas_totais.csv").then(function(dados) {
 
   const step_waterfall = function(tipo_divida, direction) {
     const bar_atual = d3.selectAll("rect.d3--endividamento").filter(d => d.tipo_divida == tipo_divida);
-    console.log("barra atual", bar_atual);
+    //console.log("barra atual", bar_atual);
     bar_atual.transition().duration(500)
     .attr("x", d => direction == "down" ? d.x_1 : d.x_0)
     .attr("fill", direction == "down" ? cor("Destaque") : cor("Total"));
   }
 
   const step_colore = function(direction) {
-    console.log("Disparei função colore, com a direção", direction);
+    //console.log("Disparei função colore, com a direção", direction);
     if (direction == "down") {
       console.log("tô dentro do ramo direction=down")
       d3.selectAll("rect.d3--endividamento")
@@ -211,8 +282,81 @@ d3.csv("dividas_totais.csv").then(function(dados) {
       .attr("y", d => direction == "down" ? d.y_2 : d.y);
   };
 
-  abertura();
-  //remove_barra_total();
+  const mostra_rotulo = function(escopo, tipo_divida, alinhamento) {
+    const pad_rotulo = 10;
+    const largura_rotulo = w_liq/4 - 2*pad_rotulo;
+
+    dados_rotulo = rotulos[escopo+"_"+tipo_divida];
+
+    const div_rotulo = $container_endividamento
+      .append("div")
+      .classed("rotulos-endiv", true)
+      .classed("rotulos-"+escopo+tipo_divida, true)
+      .style("top", dados_rotulo.y + "px")
+      .style("left", alinhamento == "left" ? dados_rotulo.x + bar_width + pad_rotulo + "px" : dados_rotulo.x - largura_rotulo - pad_rotulo + "px")
+      .style("width", largura_rotulo+"px")
+      .style("text-align", alinhamento)
+      .style("color", dados_rotulo.cor)
+      .style("opacity", 0);
+
+    div_rotulo
+      .append("p")
+      .html("<strong>"+dados_rotulo.texto+"</strong>");
+    div_rotulo
+      .append("p")
+      .text(dados_rotulo.valor);
+
+    div_rotulo.transition().duration(500).style("opacity", 1);
+  }
+
+  const remove_rotulo = function(escopo, tipo_divida) {
+    $container_endividamento
+      .select(".rotulos-"+escopo+tipo_divida)
+      .transition()
+      .duration(500)
+      .style("opacity", 0)
+      .remove();
+  }
+
+  console.log("Tipos dividas", d3.map(dados_vis, d => d.tipo_divida).keys());
+
+  // monta tabela para construir rótulos do "eixo"
+  let tipos = d3.map(dados_vis, d => d.tipo_divida).keys();
+  tipos = tipos.map(el => ({
+    rotulo : tipos_dividas[el],
+    pos_x  : (dados_vis.filter(d => d.tipo_divida == el)[0].x_1)
+  }))
+
+  const monta_rotulos_eixo = function() {
+    const largura_rotulo = w_liq/4 - 25;
+    $container_endividamento
+      .selectAll("p.rotulos-eixo-endiv")
+      .data(tipos)
+      .enter()
+      .append("p")
+      .classed("rotulos-eixo-endiv", true)
+      .text(d => d.rotulo)
+      .style("width", largura_rotulo + "px")
+      .style("font-style", "italic")
+      .style("top", y_max + "px")
+      .style("left", d => d.pos_x - largura_rotulo/2 + bar_width/2 + "px")
+      .style("text-align", "center")
+      .style("opacity", 0)
+      .transition()
+      .duration(500)
+      .style("opacity", 1);
+  }
+
+  const remove_rotulos_eixo = function() {
+    $container_endividamento
+      .selectAll("p.rotulos-eixo-endiv")
+      .transition()
+      .duration(500)
+      .style("opacity", 0)
+      .remove();
+
+  }
+
 
   // ze SCROLLER!
 
@@ -243,19 +387,48 @@ d3.csv("dividas_totais.csv").then(function(dados) {
 
       switch (response.index) {
         case 1:
-          step_waterfall("Divida_Uniao", response.direction);
+          abertura();
+          if (response.direction == "down") {
+            mostra_rotulo("Total", "Divida_Total", "left")
+          } else {
+            remove_rotulo("Total", "Divida_Total");
+            $svg_endividamento.selectAll("*")
+              .transition()
+              .duration(250)
+              .attr("opacity", 0)
+              .remove();
+          } 
           break;
         case 2:
-          step_waterfall("Divida_Garantida", response.direction);
+          step_waterfall("Divida_Uniao", response.direction);
+          if (response.direction == "down") {
+            mostra_rotulo("Total", "Divida_Uniao", "right");
+            remove_rotulo("Total", "Divida_Total");
+            remove_barra_total();
+          } else {
+            remove_rotulo("Total", "Divida_Uniao");
+          }
           break;
         case 3:
-          step_waterfall("Divida_demais", response.direction);
-          break;  
+          step_waterfall("Divida_Garantida", response.direction);
+          if (response.direction == "down") mostra_rotulo("Total", "Divida_Garantida", "right")
+          else remove_rotulo("Total", "Divida_Garantida");
+          break;
         case 4:
-          step_colore(response.direction);
-          break; 
+          step_waterfall("Divida_demais", response.direction);
+          if (response.direction == "down") mostra_rotulo("Total", "Divida_demais", "left");
+          else remove_rotulo("Total", "Divida_demais");
+          break;  
         case 5:
+          step_colore(response.direction);
+          if(response.direction == "up") remove_rotulos_eixo();
+          break; 
+        case 6:
+          remove_rotulo("Total", "Divida_Uniao");
+          remove_rotulo("Total", "Divida_Garantida");
+          remove_rotulo("Total", "Divida_demais");
           step_separa(response.direction);
+          if(response.direction == "down") monta_rotulos_eixo()
           break;                                  
       }
     })
@@ -266,37 +439,6 @@ d3.csv("dividas_totais.csv").then(function(dados) {
       .duration(200)
       .opacity(0);
     })*/
-
-
-  // escutando os botões
-
-  const botoes = d3.selectAll("nav.controls > button");
-
-  botoes.on("click", function() {
-    const step_sel = this.id;
-    switch (step_sel) {
-      case "step-1":
-        step("Divida_Uniao");
-        break;
-      case "step-2":
-        step("Divida_Garantida");
-        break;
-      case "step-3":
-        step("Divida_demais");
-        break;
-      case "step-4":
-        step_colore();
-        break;          
-      case "step-5":
-        step_final();
-        break;        
-      case "step-0":
-        reseta();
-        break;
-              
-
-    }
-  })
 
 
 });
