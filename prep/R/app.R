86d3edafd4a824c6afb03aee2bd062dc74f89e48
tiagombp <tiagombp@gmail.com>
2022-01-20 14:46:12 -0300

    adiciona código e arquivos para processamento dos dados necessários à atualização quadrimestral

---
+++ prep/R/app.R
@@ -0,0 +1,466 @@
+library(shiny)
+library(readxl)
+library(tidyr)
+library(lubridate)
+library(ggplot2)
+library(plotly)
+library(stringr)
+library(dplyr)
+library(shinydashboard)
+library(flexdashboard)
+library(bizdays)
+library(matrixStats)
+library(rlang)
+library(readr)
+library(abjutils)
+
+#Definição Pasta de Processamento dos Arquivos
+
+setwd("C:/Users/sara/Desktop/Lucas/GT-CEAD/Projetos/16. Painel Garantias - CODIV - SUDIP/R")
+
+#___________________________________________________________________________________________
+
+ numero <- function(x){
+  
+  a <- as.character(x)
+  b <- str_replace_all(a,"\\.","")
+  c <- str_replace_all(b,",","\\.")
+  d <- as.numeric(c)
+  
+  return(d)
+ }
+ 
+ #Função Para Remover Acentos
+ 
+ remove_acento <- function(str,pattern="all") {
+   if(!is.character(str))
+     str <- as.character(str)
+   
+   pattern <- unique(pattern)
+   
+   if(any(pattern=="Ç"))
+     pattern[pattern=="Ç"] <- "ç"
+   
+   symbols <- c(
+     acute = "áéíóúÁÉÍÓÚýÝ",
+     grave = "àèìòùÀÈÌÒÙ",
+     circunflex = "âêîôûÂÊÎÔÛ",
+     tilde = "ãõÃÕñÑ",
+     umlaut = "äëïöüÄËÏÖÜÿ",
+     cedil = "çÇ"
+   )
+   
+   nudeSymbols <- c(
+     acute = "aeiouAEIOUyY",
+     grave = "aeiouAEIOU",
+     circunflex = "aeiouAEIOU",
+     tilde = "aoAOnN",
+     umlaut = "aeiouAEIOUy",
+     cedil = "cC"
+   )
+   
+   accentTypes <- c("´","`","^","~","¨","ç")
+   
+   if(any(c("all","al","a","todos","t","to","tod","todo")%in%pattern)) # opcao retirar todos
+     return(chartr(paste(symbols, collapse=""), paste(nudeSymbols, collapse=""), str))
+   
+   for(i in which(accentTypes%in%pattern))
+     str <- chartr(symbols[i],nudeSymbols[i], str)
+   
+   return(str)
+ }
+ 
+ #___________________________________________________________
+ 
+##########
+
+#Novo período (Data) - Último Quadrimestre
+
+Novo_Periodo <- as.character("30abr2021") #alterar essa data para atualizar processamento dos dados
+
+#Radical Dados Garantias Totais 
+
+total <- as.character("Comp Indx ")
+
+atm_total   <- as.character("ATM IntExt ")
+
+atm_interno   <- as.character("ATM Int ")
+
+atm_externo   <- as.character("ATM Ext ")
+
+custo_total <- as.character("CustoMed IntExt ")
+
+custo_interno <- as.character("CustoMed Int ")
+
+custo_externo <- as.character("CustoMed Ext ")
+
+percentual_vincendo <- as.character("PercVinc ")
+
+credores <- as.character("EstTranche ")
+
+
+
+
+#_________________________________________________________________________________
+
+# Processamento - Garantias Totais, Interna e Externa; Data de REferência
+
+#CSV - Total de Garantias
+
+agrupador_total_bruto <- read.csv2(paste0(total,Novo_Periodo,".csv"), encoding = "LATIN1") %>%
+        rename(Inicio = 1) 
+        
+
+#Número linha agregador (definição até que linha deve ser ignorada para iniciar processamento)
+        
+linha_agrupador_total <- which(agrupador_total_bruto$Inicio=="Agrupador")
+
+#Obtenção da Data de Referência da Base de Dados
+
+data_referencia_total <- agrupador_total_bruto$Inicio[which(str_detect(agrupador_total_bruto$Inicio, "Data de Referência:"))]
+
+data_referencia <- as.Date(str_sub(data_referencia_total,21,str_length(data_referencia_total)),"%d/%m/%Y" )
+
+#Processamento da Base com total de Garantias - Formatada
+
+lista_agrupador <- c("Garantia Total","Todas","Estados", "Bancos Federais", "Municípios","Estatais Federais","Entidades Estaduais Controladas","Empresas Privatizadas")
+
+
+agrupador_total <- agrupador_total_bruto %>% 
+    filter(row_number() > linha_agrupador_total) %>%
+    rename(total_total=2,
+                         total_IPCA=4, 
+                         total_selic=6, 
+                         total_TJLP = 8, 
+                         total_TR=10,
+                         total_cambial=12,
+                         total_nao_indexado=14,
+                         interna_total=16,
+                         interna_USD=18,
+                         interna_IPCA=20,
+                         interna_selic=22,
+                         interna_TJLP=24,
+                         interna_TR=26,
+                         interna_nao_indexado=28,
+                         externa_total=30,
+                         externa_USD=32,
+                         externa_EUR=34,
+                         externa_JPY=36,
+                         externa_demais=38) %>%
+    mutate(Inicio = as.character(Inicio), 
+           Classificador = ifelse(Inicio %in% lista_agrupador,Inicio, NA),
+           Periodo = data_referencia,
+           Grupo = as.character("Total")) %>%
+    select(Inicio, Classificador,Grupo,Periodo,everything()) %>%
+    fill(Classificador) %>%
+    mutate(Inicio = rm_accent(Inicio)) %>%
+    mutate(Classificador = rm_accent(Classificador))
+#___________________________________________________________________________________________________________    
+    
+# Processamento - ATM (Avarage Time to Maturity) - TOTAl
+
+#CSV - ATM Total
+
+agrupador_atm_bruto <- read.csv2(paste0(atm_total,Novo_Periodo,".csv"), encoding = "LATIN1") %>%
+    rename(Inicio = 1) 
+
+#Número linha agregador (definição até que linha deve ser ignorada para iniciar processamento)
+
+linha_agrupador_atm <- which(agrupador_atm_bruto$Inicio=="Agrupador")
+
+#Processamento da Base com ATM das Garantias - Formatada
+
+agrupador_atm <- agrupador_atm_bruto %>% 
+    filter(row_number() > linha_agrupador_atm) %>%
+    rename(ATM_Total = 2,
+           Financeiro_atm_total = 3) %>%
+    mutate(Inicio = as.character(Inicio), 
+           Classificador = ifelse(Inicio %in% lista_agrupador,Inicio, NA),
+           Periodo = data_referencia,
+           Grupo = as.character("ATM")) %>%
+    select(Inicio, Classificador,Grupo,Periodo,everything()) %>%
+    fill(Classificador)
+    
+# Processamento - ATM (Avarage Time to Maturity) - INTERNO
+
+#CSV - ATM Garantias
+
+agrupador_atm_bruto_interno <- read.csv2(paste0(atm_interno,Novo_Periodo,".csv"), encoding = "LATIN1") %>%
+    rename(Inicio = 1) 
+
+#Número linha agregador (definição até que linha deve ser ignorada para iniciar processamento)
+
+linha_agrupador_atm_interno <- which(agrupador_atm_bruto_interno$Inicio=="Agrupador")
+
+#Processamento da Base com ATM das Garantias - Formatada
+
+agrupador_atm_interno <- agrupador_atm_bruto_interno %>% 
+    filter(row_number() > linha_agrupador_atm_interno) %>%
+    rename(ATM_interno = 2,
+           Financeiro_atm_interno = 3) %>%
+    mutate(Inicio = as.character(Inicio), 
+       Classificador = ifelse(Inicio %in% lista_agrupador,Inicio, NA),
+       Periodo = data_referencia,
+       Grupo = as.character("ATM")) %>%
+    select(Inicio, Classificador,Grupo,Periodo,everything()) %>%
+    fill(Classificador)
+
+# Processamento - ATM (Avarage Time to Maturity) - Externo
+
+#CSV - ATM Garantias
+
+agrupador_atm_bruto_externo <- read.csv2(paste0(atm_externo,Novo_Periodo,".csv"), encoding = "LATIN1") %>%
+    rename(Inicio = 1) 
+
+#Número linha agregador (definição até que linha deve ser ignorada para iniciar processamento)
+
+linha_agrupador_atm_externo <- which(agrupador_atm_bruto_externo$Inicio=="Agrupador")
+
+#Processamento da Base com ATM das Garantias - Formatada
+
+agrupador_atm_externo <- agrupador_atm_bruto_externo %>% 
+    filter(row_number() > linha_agrupador_atm_externo) %>%
+    rename(ATM_externo = 2,
+           Financeiro_atm_externo = 3) %>%
+    mutate(Inicio = as.character(Inicio), 
+           Classificador = ifelse(Inicio %in% lista_agrupador,Inicio, NA),
+           Periodo = data_referencia,
+           Grupo = as.character("ATM")) %>%
+    select(Inicio, Classificador,Grupo,Periodo,everything()) %>%
+    fill(Classificador)
+
+# ATM Base Completa Final - Total, Interno e Externo
+
+agrupador_atm_completo <- agrupador_atm %>%
+    left_join(agrupador_atm_interno) %>%
+    left_join(agrupador_atm_externo) %>%
+    mutate(Inicio = rm_accent(Inicio)) %>%
+    mutate(Classificador = rm_accent(Classificador))
+#_______________________________________________________________________________________________________
+
+#Processamento Custo Médio
+
+#__________________________________________
+
+#CSV - CUSTO Total
+
+agrupador_custo_bruto <- read.csv2(paste0(custo_total,Novo_Periodo,".csv"), encoding = "LATIN1") %>%
+    rename(Inicio = 1) 
+
+#Número linha agregador (definição até que linha deve ser ignorada para iniciar processamento)
+
+linha_agrupador_custo <- which(agrupador_custo_bruto$Inicio=="Agrupador")
+
+#Processamento da Base com Custo das Garantias - Formatada
+
+agrupador_custo <- agrupador_custo_bruto %>% 
+    filter(row_number() > linha_agrupador_custo) %>%
+    rename(Custo_Total = 2,
+           Financeiro_custo_total = 3) %>%
+    mutate(Inicio = as.character(Inicio), 
+           Classificador = ifelse(Inicio %in% lista_agrupador,Inicio, NA),
+           Periodo = data_referencia,
+           Grupo = as.character("Custo")) %>%
+    select(Inicio, Classificador,Grupo,Periodo,everything()) %>%
+    fill(Classificador)
+
+#____________________________
+
+#CSV - CUSTO Interno
+
+agrupador_custo_bruto_interno <- read.csv2(paste0(custo_interno,Novo_Periodo,".csv"), encoding = "LATIN1") %>%
+    rename(Inicio = 1) 
+
+#Número linha agregador (definição até que linha deve ser ignorada para iniciar processamento)
+
+linha_agrupador_custo_interno <- which(agrupador_custo_bruto_interno$Inicio=="Agrupador")
+
+#Processamento da Base com Custo das Garantias - Formatada
+
+agrupador_custo_interno <- agrupador_custo_bruto_interno %>% 
+    filter(row_number() > linha_agrupador_custo_interno) %>%
+    rename(Custo_Interno = 2,
+           Financeiro_custo_interno = 3) %>%
+    mutate(Inicio = as.character(Inicio), 
+           Classificador = ifelse(Inicio %in% lista_agrupador,Inicio, NA),
+           Periodo = data_referencia,
+           Grupo = as.character("Custo")) %>%
+    select(Inicio, Classificador,Grupo,Periodo,everything()) %>%
+    fill(Classificador)
+
+#____________________________
+
+#CSV - CUSTO Externo
+
+agrupador_custo_bruto_externo <- read.csv2(paste0(custo_externo,Novo_Periodo,".csv"), encoding = "LATIN1") %>%
+    rename(Inicio = 1) 
+
+#Número linha agregador (definição até que linha deve ser ignorada para iniciar processamento)
+
+linha_agrupador_custo_externo <- which(agrupador_custo_bruto_externo$Inicio=="Agrupador")
+
+#Processamento da Base com Custo das Garantias - Formatada
+
+agrupador_custo_externo <- agrupador_custo_bruto_externo %>% 
+    filter(row_number() > linha_agrupador_custo_externo) %>%
+    rename(Custo_Externo = 2,
+           Financeiro_custo_externo = 3) %>%
+    mutate(Inicio = as.character(Inicio), 
+           Classificador = ifelse(Inicio %in% lista_agrupador,Inicio, NA),
+           Periodo = data_referencia,
+           Grupo = as.character("Custo")) %>%
+    select(Inicio, Classificador,Grupo,Periodo,everything()) %>%
+    fill(Classificador)
+
+#______________________________
+
+# Custo Base Completa Final - Total, Interno e Externo
+
+agrupador_custo_completo <- agrupador_custo %>%
+    left_join(agrupador_custo_interno) %>%
+    left_join(agrupador_custo_externo) %>%
+     mutate(Inicio = rm_accent(Inicio)) %>%
+  mutate(Classificador = rm_accent(Classificador))
+
+#_______________________________________________________________________________________________________
+
+#Processamento Percentual Vincendo
+
+#__________________________________________
+
+#CSV - Percentual Vincendo
+
+agrupador_percentual_vincendo_bruto <- read.csv2(paste0(percentual_vincendo,Novo_Periodo,".csv"), encoding = "LATIN1") %>%
+    rename(Inicio = 1) 
+
+#Número linha agregador (definição até que linha deve ser ignorada para iniciar processamento)
+
+linha_agrupador_percentual_vincendo <- which(agrupador_percentual_vincendo_bruto$Inicio=="Agrupador")
+
+#Processamento da Base com Percentual Vincendo das Garantias - Formatada
+
+agrupador_percentual_vincendo <- agrupador_percentual_vincendo_bruto %>% 
+    filter(row_number() > linha_agrupador_percentual_vincendo) %>%
+    #rename(Custo_Total = 2,
+     #      Financeiro_custo_total = 3) %>%
+    mutate(Inicio = as.character(Inicio), 
+           Classificador = ifelse(Inicio %in% lista_agrupador,Inicio, NA),
+           Periodo = data_referencia,
+           Grupo = as.character("Custo")) %>%
+    select(Inicio, Classificador,Grupo,Periodo,everything()) %>%
+    fill(Classificador) %>%
+    rename (Ate_12_meses = 5,
+            Ate_12_meses_percentual = 6,
+            De_1_2_anos = 7,
+            De_1_2_anos_percentual = 8,
+            De_2_3_anos = 9,
+            De_2_3_anos_percentual = 10,
+            De_3_4_anos = 11,
+            De_3_4_anos_percentual = 12,
+            De_4_5_anos = 13,
+            De_4_5_anos_percentual = 14,
+            Acima_5_anos = 15,
+            Acima_5_anos_percentual = 16,
+            Total = 17,
+            Total_percentual = 18
+    ) %>%
+  mutate(Inicio = rm_accent(Inicio)) %>%
+  mutate(Classificador = rm_accent(Classificador))
+
+
+#Processamento Credores
+
+#__________________________________________
+
+#CSV - Credores
+
+agrupador_credores_bruto <- read.csv2(paste0(credores,Novo_Periodo,".csv"), encoding = "LATIN1") %>%
+  rename(Inicio = 1) 
+
+#Número linha agregador (definição até que linha deve ser ignorada para iniciar processamento)
+
+linha_agrupador_credores <- which(agrupador_credores_bruto$Inicio=="Nome do Contrato")
+
+#Processamento da Base com Credores - Formatados
+
+agrupador_credores <- agrupador_credores_bruto %>% 
+  filter(row_number() > linha_agrupador_credores)%>%
+  rename(Tipo_Divida = 4, 
+         Mutuario = 5,
+         Tipo_Mutuario = 6,
+         UF = 7,
+         Credor = 11,
+         Saldo_Principal = 24) %>%
+  select(Tipo_Mutuario, Mutuario, UF, Credor, Tipo_Divida,Saldo_Principal) %>%
+   mutate(
+     Saldo_Principal = numero(Saldo_Principal),
+     Mutuario = remove_acento(Mutuario),
+     Tipo_Mutuario = remove_acento(Tipo_Mutuario)
+   ) 
+  
+
+  #filter(grepl("Toledo", Mutuario)) %>%
+  #group_by(Credor, Mutuario, Tipo_Divida, Tipo_Mutuario) %>%
+  #summarise(Saldo_Principal = sum(Saldo_Principal))
+
+#__________________________________________
+
+#CSV - Honras Garantias
+
+honras <- read_delim("Relatorio_honras_atrasos 30abr2021.csv", 
+                     ";", escape_double = FALSE, locale = locale(date_format = "%d/%m/%Y", 
+                                                                 decimal_mark = ",", grouping_mark = ".", 
+                                                                 encoding = "LATIN1"), trim_ws = TRUE, 
+                     skip = 10)
+
+
+#_________________________________________________
+
+#CSV - Novos Contratos
+
+novos_contratos <- read_delim("InfCadastrais 30abr2021 - Final.csv", 
+                     ";", escape_double = TRUE, locale = locale(date_format = "%d/%m/%y", 
+                                                                 decimal_mark = ",", grouping_mark = ".", 
+                                                                 encoding = "LATIN1"), trim_ws = TRUE, 
+                     
+                     skip = 10)  
+  
+                    
+
+
+novos_contratos <- novos_contratos %>%
+                   mutate(Mutuario = rm_accent(`Mutuário`),
+                          `Valor Contratado Original` = (`Valor Contratado Original`/1000000),
+                          Assinatura = as.Date(`Data de Assinatura`, origin="1899-12-30", format ="%d/%m/%Y"),
+                          Ano = year(Assinatura),
+                          Status = ifelse(Fase == "Concluído","Concluido","Ativo")) %>%
+                   mutate(Tipo_Mutuario = rm_accent(`Tipo Mutuário`)) %>%
+                   rename(Moeda = 22,
+                          Tipo = 9 )
+                   
+novos_contratos$Tipo_Mutuario <- ifelse(novos_contratos$Tipo_Mutuario =="Entidades Estaduais Controladas", 
+                          "Entidades Controladas",
+                          novos_contratos$Tipo_Mutuario)
+#___________________________
+
+
+
+#____________________________
+
+save(list = c("data_referencia","honras","novos_contratos","agrupador_atm_completo","agrupador_custo_completo","agrupador_percentual_vincendo","agrupador_total", "agrupador_credores"),file = "Garantias_abr_2021.Rdata")
+
+#save(list = c("honras"),file = "Honras_mai_2020.Rdata")
+
+
+#____________________________________________________________________________________________________________
+
+    
+
+ui <- fluidPage(
+)
+
+server <- function(input, output) {
+}
+
+
+shinyApp(ui = ui, server = server)
---
+++ prep/R/prepara_dados_vis.R
@@ -0,0 +1,164 @@
+library(tidyverse)
+library(lubridate)
+library(stringr)
+library(viridis)
+library(colorspace)
+library(extrafont)
+
+#extrafont::font_import()
+loadfonts()
+
+load("R/Garantias.Rdata")
+
+
+# Dados Bubble Chart ------------------------------------------------------
+
+dados_vis_pre <- agrupador_total %>%
+  filter(Periodo == max(Periodo),
+         Inicio != Classificador) %>%
+  select(Inicio, Classificador, valor = total_total) %>%
+  mutate(valor = as.numeric(
+    str_replace(str_replace_all(as.character(valor), "\\.", ""), ",", "\\.")),
+    rank_geral = rank(-valor))
+
+dados_vis <- dados_vis_pre %>%
+  group_by(Classificador) %>%
+  summarise(total_classificador = sum(valor)) %>%
+  ungroup() %>%
+  mutate(rank_classificadores = rank(-total_classificador)) %>%
+  right_join(dados_vis_pre)
+
+write.csv(dados_vis, file = "webpage/dados_vis.csv", fileEncoding = "UTF-8")
+
+dados_vis %>% group_by(Classificador) %>% summarize(first(total_classificador))
+
+dados_vis %>% filter(rank_geral>=16) %>% group_by() %>% summarise(sum(valor))
+
+
+# Dados Quadro ------------------------------------------------------------
+
+quadro <- list(
+  
+  agrupador_total %>%
+    select(Inicio, Classificador, Periodo,
+           total_total,
+           interna_cambial = interna_USD, 
+           interna_total, 
+           externa_total),
+  
+  agrupador_atm_completo %>%
+    select(Inicio, Classificador, Periodo,
+           ATM_Total),
+  
+  agrupador_custo_completo %>%
+    select(Inicio, Classificador, Periodo,
+           Custo_Total),
+  
+  agrupador_percentual_vincendo %>%
+    select(Inicio, Classificador, Periodo,
+           Ate_12_meses, Ate_12_meses_percentual, 
+           De_1_2_anos, De_1_2_anos_percentual, 
+           De_2_3_anos, De_2_3_anos_percentual, 
+           De_3_4_anos, De_3_4_anos_percentual, 
+           De_4_5_anos, De_4_5_anos_percentual, 
+           Acima_5_anos, Acima_5_anos_percentual)) %>%
+  
+  reduce(full_join, by = c("Inicio", "Classificador", "Periodo")) %>%
+  # juntamos todos, agora as limpezas
+  filter(Periodo == max(Periodo),
+         Inicio != Classificador) %>%
+  mutate(Custo_Total = as.character(Custo_Total)) %>%
+  mutate_at(.vars = vars(ends_with("_percentual")), .funs = ~as.character(.)) %>%
+  mutate_if(is.factor, .funs = ~as.numeric(
+    str_replace(
+      str_replace_all(
+        as.character(.), "\\.", ""), ",", "\\."))) %>%
+  mutate(interna_demais = interna_total - interna_cambial)
+
+write.csv(quadro, file = "webpage/dados_quadro.csv", fileEncoding = "UTF-8")
+
+
+# honras ------------------------------------------------------------------
+
+honras_simples <- honras %>%
+  select(data = `Data de Vencimento`,
+         tipo_divida = `Tipo de Dívida`,
+         `Credor`,
+         tipo_credor = `Classificação do Credor`,
+         mutuario = `Mutuário`,
+         tipo_mutuario = `Tipo de Mutuário`,
+         `Status`,
+         valor = `Honra - Total (R$)`) %>%
+  as.data.frame() %>%
+  mutate(mes = str_pad(month(data), width = 2, pad = "0"),
+         ano = year(data),
+         mes_ano = paste0(ano,mes))
+
+honras_plot <- honras_simples %>%
+  group_by(mes_ano, mutuario) %>%
+  summarise(valor = sum(valor)) %>%
+  ungroup() %>%
+  arrange(mes_ano) %>%
+  mutate(data = as.Date(paste(str_sub(mes_ano, 1, 4),
+                      str_sub(mes_ano, 5, 6),
+                      "01", sep = "-"))) %>%
+  group_by(mutuario) %>%
+  mutate(total_mutuario = sum(valor)) %>%
+  ungroup() %>%
+  mutate(ente = fct_reorder(mutuario, total_mutuario))
+
+#%>% filter(!(mutuario %in% c("Rio de Janeiro", "Minas Gerais", "Goiás"))
+
+ggplot(honras_plot, aes(y = valor, x = data)) + 
+  geom_col() + 
+  coord_polar(theta = "x") +
+  scale_x_date(limits = c(as.Date("2016-01-01"), NA), date_breaks = "1 year",
+               date_labels = "%Y") +
+  scale_y_continuous(limits = c(-4e8, NA)) +
+  geom_hline(yintercept = 0, color = "lightgrey") +
+  facet_wrap(~mutuario) +
+  theme_minimal() +
+  theme(axis.line = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        panel.grid = element_blank())
+
+ggplot(honras_plot) + geom_histogram(aes(valor), bins = 100)
+
+summary(honras_plot$valor)
+
+ggplot(honras_plot, aes(y = mutuario, x = mes_ano)) + geom_point()
+
+plot_honras1 <- ggplot(honras_plot, aes(y = ente, x = data, fill = valor)) + 
+  geom_tile(width = 30, height = 1, color = "white", size = 1) +
+  # geom_text(x = max(honras_plot$data) + 90, y = length(unique(honras_plot$mutuario)) + 1, label = "Totais", 
+  #           family = "Merriweather Sans Light", size = 2.5, 
+  #           hjust = 0, fontface = "bold", vjust = 1) + 
+  # geom_text(x = max(honras_plot$data) + 90, 
+  #           aes(label = format(round(total_mutuario/1e6, 0), big.mark = ".",
+  #                              decimal.mark = ",")),
+  #           family = "Merriweather Sans Light", size = 2.5, 
+  #           hjust = "left", check_overlap = TRUE, vjust = "bottom", angle = 60) +
+  scale_x_date(limits = c(as.Date("2015-12-01"), NA), date_breaks = "4 months",
+               date_labels = "%b\n%Y") +
+  scale_fill_continuous_sequential(palette = "BluGrn", 
+                                   labels = function(x){format(x/1e6, big.mark = ".", decimal.mark = ",")}) +
+  #scale_fill_viridis_c(direction = -1) +
+  coord_flip(clip = "off") +
+  labs(fill = NULL) +
+  theme_minimal() +
+  theme(axis.line = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title = element_blank(),
+        axis.text.y = element_text(size = 6),
+        axis.text.x = element_text(size = 6, angle = -60, vjust = 0, hjust = 0),
+        panel.grid = element_blank(),
+        legend.position = "right",
+        text = element_text(family = "Merriweather Sans Light"),
+        legend.text = element_text(size = 7),
+        plot.margin = margin(.75, 1.5, .25, .25, "cm"))
+
+ggsave(filename = "honras1.png", plot = plot_honras1, width = 4, height = 10, bg = "transparent")
+
+honras_simples %>% group_by(mutuario, tipo_mutuario) %>% summarise(sum(valor), n())
